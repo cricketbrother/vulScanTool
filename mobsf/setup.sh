acae12ca128abe25ac6c2b332e53f034c952567b
cricketbrother <xxx@xxx.com>
2024-01-11 13:33:35 +0800

    fix bugs

--- mobsf/setup.sh
+++ mobsf/setup.sh
@@ -5,6 +5,7 @@ if [ -d "Mobile-Security-Framework-MobSF" ]; then
     echo ">>> Updating Mobile-Security-Framework-MobSF"
     cd "Mobile-Security-Framework-MobSF"
     git pull
+    cd ..
 else
     echo ">>> Cloning Mobile-Security-Framework-MobSF"
     git clone https://github.com/MobSF/Mobile-Security-Framework-MobSF.git
