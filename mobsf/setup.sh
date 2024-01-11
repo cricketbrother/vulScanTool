a4ef0a9dea985df1781d8ec229f7a170563f400c
cricketbrother <xxx@xxx.com>
2024-01-11 11:25:35 +0800

    fix bugs

--- mobsf/setup.sh
+++ mobsf/setup.sh
@@ -1,14 +1,17 @@
-!#/bin/bash
+#!/bin/bash
 
 # Clone or update the repository
 if [ -d "Mobile-Security-Framework-MobSF" ]; then
+    echo ">>> Updating Mobile-Security-Framework-MobSF"
     cd "Mobile-Security-Framework-MobSF"
     git pull
 else
+    echo ">>> Cloning Mobile-Security-Framework-MobSF"
     git clone https://github.com/MobSF/Mobile-Security-Framework-MobSF.git
 fi
 
 # Install
+echo ">>> Installing MobSF"
 docker compose up
 
 # Run
